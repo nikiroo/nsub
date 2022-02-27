dff03fc5a4efe9fcc1c7457991ce3d9d09ace50a
Niki Roo <niki@nikiroo.be>
2022-02-27 23:47:38 +0100

    date typo

--- src/utils/cstring.c
+++ src/utils/cstring.c
@@ -1,7 +1,7 @@
 /*
  * CUtils: some small C utilities
  *
- * Copyright (C) 2013 Niki Roo
+ * Copyright (C) 2012 Niki Roo
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,7 +1,7 @@
 /*
  * CUtils: some small C utilities
  *
- * Copyright (C) 2022 Niki Roo
+ * Copyright (C) 2020 Niki Roo
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
